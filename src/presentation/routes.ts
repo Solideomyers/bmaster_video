212b512ed10fe86516146ab7b9965a72f7dc5fa1
Solideomyers <franciscomyers@gmail.com>
2024-01-30 20:51:09 +0100

    feat: added route auth

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { UserRoutes } from '../user/user.routes';
+import { AuthRoutes } from '../authentication/auth.routes';
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     const userRoutes = new UserRoutes();
-    router.use('/', userRoutes.getRouter());
+    const authRoutes = new AuthRoutes();
+
+    router.use('/user', userRoutes.getRouter());
+    router.use('/auth', authRoutes.getRouter());
 
     return router;
   }
