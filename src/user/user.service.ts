237ea63c11311beb36bc60be92a4974f4d7de138
Solideomyers <franciscomyers@gmail.com>
2024-01-30 18:44:33 +0100

    feat: added other features

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { CustomError } from '../errors/custom.errors';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 export class UserService {
   constructor(private userRepository: Repository<UserEntity>) {}
@@ -13,8 +15,55 @@ export class UserService {
     user.password = userDto.password;
     user.userName = userDto.email;
 
+    const userExist = await this.userRepository.findOne({
+      where: { email: userDto.email },
+    });
+    if (userExist) {
+      throw CustomError.badRequest;
+    }
+
     await this.userRepository.save(user);
 
     return user;
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return await this.userRepository.find();
+  }
+
+  async getUserById(id: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { id: id } });
+
+    if (!user) {
+      throw CustomError.notFound('User not found');
+    }
+
+    return user;
+  }
+
+  async updateUser(id: number, updateDto: UpdateUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { id: id } });
+    if (!user) {
+      throw CustomError.notFound('User no encontrado');
+    }
+
+    if (updateDto.name) {
+      user.name = updateDto.name;
+    }
+
+    if (updateDto.password) {
+      user.password = updateDto.password;
+    }
+
+    return await this.userRepository.save(user);
+  }
+
+  async deleteUser(id: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { id: id } });
+    if (!user) {
+      throw CustomError.notFound('User not found');
+    }
+
+    return await this.userRepository.remove(user);
   }
 }
