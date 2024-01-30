01da142f34a35b8a024e371b5a03bf5eb60a4409
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-30 12:19:35 -0400

    fix: refactor

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,25 +1,17 @@
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { CustomError } from '../errors/custom.errors';
 
 export class UserService {
   constructor(private userRepository: Repository<UserEntity>) {}
 
   async createUser(userDto: CreateUserDto) {
+    // Crear una nueva instancia de UserEntity a partir de los datos del DTO
     const user = new UserEntity();
     user.name = userDto.name;
     user.email = userDto.email;
     user.password = userDto.password;
     user.userName = userDto.email;
-
-    const userExist = await this.userRepository.findOne({
-      where: { email: userDto.email },
-    });
-
-    if (userExist) {
-      throw new CustomError(400, 'User exist');
-    }
 
     await this.userRepository.save(user);
 
