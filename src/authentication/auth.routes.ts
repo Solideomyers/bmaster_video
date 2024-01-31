083c083879945ce07d3b61dc5d5b3b84d82aacda
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-31 04:51:28 -0400

    feat: add auth routes

--- src/authentication/auth.routes.ts
+++ src/authentication/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { AppDataSource } from '../data-source/data-source';
 import { UserEntity } from '../user/entities/user.entity';
 
@@ -8,14 +9,43 @@ export class AuthRoutes {
   private authController: AuthController;
 
   constructor() {
-    const userRepository = AppDataSource.getRepository(UserEntity);
     this.router = Router();
-    this.authController = new AuthController(userRepository);
+    const userRepository = AppDataSource.getRepository(UserEntity);
+    this.authController = new AuthController(new AuthService(userRepository));
 
     this.initializeRoutes();
   }
 
   private initializeRoutes(): void {
+    /**
+     * @swagger
+     * /auth/signup:
+     *   post:
+     *     summary: Sign up
+     *     description: Endpoint to sign up and obtain a JWT token
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/CreateUserDto'
+     *     responses:
+     *       '201':
+     *         description: Token generated successfully
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 token:
+     *                   type: string
+     *       '500':
+     *         description: Internal server error
+     */
+    this.router.post('/signup', async (req, res) => {
+      await this.authController.signup(req, res);
+    });
+
     /**
      * @swagger
      * /auth/signin:
@@ -29,7 +59,7 @@ export class AuthRoutes {
      *           schema:
      *             type: object
      *             properties:
-     *               username:
+     *               email:
      *                 type: string
      *               password:
      *                 type: string
@@ -44,39 +74,12 @@ export class AuthRoutes {
      *                 token:
      *                   type: string
      *       '401':
-     *         description: Unauthorized, invalid credentials
+     *         description: Invalid email or password
+     *       '500':
+     *         description: Internal server error
      */
     this.router.post('/signin', async (req, res) => {
       await this.authController.signin(req, res);
-    });
-
-    /**
-     * @swagger
-     * /auth/signup:
-     *   post:
-     *     summary: Sign up
-     *     description: Endpoint to sign up and obtain a JWT token
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             $ref: '#/components/schemas/CreateUserDto'
-     *     responses:
-     *       '200':
-     *         description: Token generated successfully
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 token:
-     *                   type: string
-     *       '400':
-     *         description: Bad request, invalid user data
-     */
-    this.router.post('/signup', async (req, res) => {
-      await this.authController.signup(req, res);
     });
   }
 
