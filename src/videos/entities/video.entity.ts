bf01e73732293dbdf2b934e1f6e24e186c538471
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-31 04:45:04 -0400

    feat: add video entity

---
+++ src/videos/entities/video.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { UserEntity } from '../../user/entities/user.entity';
+import { LikeEntity } from '../../likes/entities/like.entity';
+
+@Entity('videos')
+export class VideoEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.videos)
+  user: UserEntity;
+
+  @Column({ default: 0 })
+  numLikes: number;
+
+  @Column()
+  title: string;
+
+  @Column({ length: 200, unique: true })
+  url: string;
+
+  @Column()
+  description: string;
+
+  @Column({ type: 'enum', enum: ['public', 'private'] })
+  view: string;
+
+  @OneToMany(() => LikeEntity, (like) => like.video)
+  likes: LikeEntity[];
+}
